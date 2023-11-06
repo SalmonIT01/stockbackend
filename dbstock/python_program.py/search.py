aac0468211715c513868addb0a7b00437c51a9a1
SalmonIT01 <101684618+SalmonIT01@users.noreply.github.com>
2023-11-06 15:27:37 +0700

    add convert_time,convert_unit,delete function

--- dbstock/python_program.py/search.py
+++ dbstock/python_program.py/search.py
@@ -3,6 +3,21 @@ import sqlite3
 con = sqlite3.connect('stockdb.db')
 cursur = con.cursor()
 
+def insert():
+    print("INSERT")
+    input_id = input('product id:')
+    input_title = input('product name')
+    input_unit = input('unit:')
+    convert_unit = convert(input_unit)
+    input_amount = float(input('amount:'))
+    insert_sql = '''INSERT INTO products2 (product_id, product_name,unit_id, amount)
+                     VALUES("{0}","{1}","{2}","{3}")'''.format(input_id,input_title,convert_unit,input_amount)
+    #
+    cursur.execute(insert_sql)
+    con.commit()
+    convert_time()
+    return input_unit
+
 def search():
     user_input = input('Search:')
     try:
@@ -13,27 +28,13 @@ def search():
     except :
         search_sql = '''SELECT * FROM products2
         WHERE product_name LIKE "%{0}%" '''.format(user_input)
+        
+    print(search_sql)
 
     cursur.execute(search_sql)
     result = cursur.fetchall()
     print(result)
    
-
-def insert_products():
-    print("INSERT")
-    input_id = input('product id:')
-    input_title = input('product name')
-    input_unit = int(input('unit:'))
-    input_amount = float(input('amount:'))
-    
-    insert_sql = '''INSERT INTO products2 (product_id, product_name, unit_id, amount)
-                     VALUES("{0}","{1}","{2}","{3}")'''.format(input_id,input_title,input_unit,input_amount)
-    
-    cursur.execute(insert_sql)
-    con.commit()
-    convert_time(input_id) #function convert time
-    showdb()
-    
 def showdb():
     db = '''SELECT * 
             FROM products2; '''
@@ -56,6 +57,47 @@ def updatedb():
     cursur.execute(update_sql)
     con.commit()
     
+def delete():
+        deleteuser_input = input('select data to delete[product id]:')
+        try:
+            int(deleteuser_input)
+            userinput_sql = '''SELECT product_id FROM products2'''
+            cursur.execute(userinput_sql)
+            result = cursur.fetchall()
+            list_infomation=[]
+            for c in result :
+                list_infomation.append(list(c)[0])
+            str_information = str(deleteuser_input)
+            if str_information in list_infomation :
+                bool_input = input("Are you sure you want to delete(Y/N): ")
+                if bool_input == "Y" :
+                    delete_sql = '''
+                    DELETE FROM products2
+                    WHERE product_id = "{0}" ;
+                    '''.format(deleteuser_input)
+                    cursur.execute(delete_sql)
+                    con.commit()
+                    print("Delete complete.")
+                elif bool_input == "N" :
+                    print("OK system isn't delete your information.")
+            else :
+                print("Don't have this product id.")
+        
+        except :
+            print("invalid input must to be integer or don't have in order.")
+            delete()
+
+def convert(cv):#เปลี่ยน รีม เป็น 3
+
+    unitID = '''select id from unit_table
+         where title_unit GLOB  "{0}"'''.format(cv)
+    cursur.execute(unitID)
+    result = cursur.fetchall()
+    x= list(result[0])
+    return x[0]
+
+
+    
 def convert_time(p_id):
     sql_date = '''
             select datetime(create_date, 'localtime')
@@ -73,16 +115,36 @@ def convert_time(p_id):
     
     cursur.execute(update)
     con.commit()
+
+def status_update():
+    get_sql = '''SELECT product_id , amount , status_id FROM products2''' # get product_id, amount, status_id
+    cursur.execute(get_sql)
+    result = cursur.fetchall()
+    for a in result:
+        amount = a[1]
+        if amount > 0 :
+            update_sql = '''
+            UPDATE products2
+            set status_id = 1
+            WHERE product_id = "{0}";'''.format(a[0])
+        if amount <= 0 :
+            update_sql = '''
+            UPDATE products2
+            set status_id = 0
+            WHERE product_id = "{0}";'''.format(a[0])
+        cursur.execute(update_sql)
+        con.commit()
     
 
     
     
     
     
-    
+   
 # search()    
-
 # showdb()  
 # insert()  
 # updatedb()
-convert_time()
+# status_update()
+# delete()
+
